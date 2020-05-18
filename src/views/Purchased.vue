bde43f4f1e7e10f646ad4a141562ab2c7973fc09
brianleect <brianleecheowteng@gmail.com>
2020-05-18 12:14:40 +0800

    Sync initial vue server

---
+++ src/views/Purchased.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h3>Succeded</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
