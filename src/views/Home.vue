b8644970aaa15034771be5c42a2310554aaaa416
brianleect <brianleecheowteng@gmail.com>
2020-05-20 01:51:36 +0800

    Fixed checkout null error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,6 +35,110 @@
   </div>
 </template>
 
+<script src="https://js.stripe.com/v3"></script>
+
+<script>
+(function() {
+  var stripe = Stripe("pk_test_LIc2NCzFeOD5ng6VrGwNE8Dx00Z67P4mCD");
+
+  var checkoutButton = document.getElementById(
+    "checkout-button-plan_HED8EQd8RL0AN3"
+  );
+  checkoutButton.addEventListener("click", function() {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe
+      .redirectToCheckout({
+        lineItems: [{ price: "plan_HED8EQd8RL0AN3", quantity: 1 }],
+        mode: "subscription",
+        // Do not rely on the redirect to the successUrl for fulfilling
+        // purchases, customers may not always reach the success_url after
+        // a successful payment.
+        // Instead use one of the strategies described in
+        // https://stripe.com/docs/payments/checkout/fulfillment
+        successUrl: "http://localhost:8080/#/purchased",
+        cancelUrl: "http://localhost:8080/#/cancelled"
+      })
+      .then(function(result) {
+        if (result.error) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer.
+          var displayError = document.getElementById("error-message");
+          displayError.textContent = result.error.message;
+        }
+      });
+  });
+})();
+</script>
+
+<script>
+(function() {
+  var stripe = Stripe("pk_test_LIc2NCzFeOD5ng6VrGwNE8Dx00Z67P4mCD");
+
+  var checkoutButton2 = document.getElementById(
+    "checkout-button-plan_HED9jNsqacP8sZ"
+  );
+  checkoutButton2.addEventListener("click", function() {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe
+      .redirectToCheckout({
+        lineItems: [{ price: "plan_HED9jNsqacP8sZ", quantity: 1 }],
+        mode: "subscription",
+        // Do not rely on the redirect to the successUrl for fulfilling
+        // purchases, customers may not always reach the success_url after
+        // a successful payment.
+        // Instead use one of the strategies described in
+        // https://stripe.com/docs/payments/checkout/fulfillment
+        successUrl: "http://localhost:8080/#/purchased",
+        cancelUrl: "http://localhost:8080/#/cancelled"
+      })
+      .then(function(result) {
+        if (result.error) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer.
+          var displayError = document.getElementById("error-message");
+          displayError.textContent = result.error.message;
+        }
+      });
+  });
+})();
+</script>
+
+<script>
+(function() {
+  var stripe = Stripe("pk_test_LIc2NCzFeOD5ng6VrGwNE8Dx00Z67P4mCD");
+
+  var checkoutButton3 = document.getElementById(
+    "checkout-button-sku_HEW2W2BNinxyYk"
+  );
+  checkoutButton3.addEventListener("click", function() {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe
+      .redirectToCheckout({
+        lineItems: [{ price: "sku_HEW2W2BNinxyYk", quantity: 1 }],
+        mode: "payment",
+        // Do not rely on the redirect to the successUrl for fulfilling
+        // purchases, customers may not always reach the success_url after
+        // a successful payment.
+        // Instead use one of the strategies described in
+        // https://stripe.com/docs/payments/checkout/fulfillment
+        successUrl: "http://localhost:8080/#/purchased",
+        cancelUrl: "http://localhost:8080/#/cancelled"
+      })
+      .then(function(result) {
+        if (result.error) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer.
+          var displayError = document.getElementById("error-message");
+          displayError.textContent = result.error.message;
+        }
+      });
+  });
+})();
+</script>
+
 <script>
 export default {
   name: "Home",
